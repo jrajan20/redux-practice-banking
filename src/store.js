6c0850ec92b2e3c975a86fa8121f421cd4ac7fed
jrajan20 <jai_20j@hotmail.com>
2026-07-04 07:25:10 -0400

    added redux, loan cases and experimenting with dispatch actions

--- src/store.js
+++ src/store.js
@@ -1,3 +1,5 @@
+import { createStore } from "redux";
+
 const initialState = {
   balance: 0,
   loan: 0,
@@ -17,13 +19,30 @@ function reducer(state = initialState, action) {
         ...state,
         balance: state.balance - action.payload
       };
-    case "account/applyLoan":
+    case "account/requestLoan":
+    if (state.loan > 0) return state;
       return {
         ...state,
         loan: action.payload.amount,
         loanPurpose: action.payload.purpose
+        balance: state.balance + action.payload.amount,
       };
+      case "account/payLoan":
+        return {
+            ...state,
+              loan: 0,
+            loanPurpose: "",
+            balance: state.balance - state.loan,
+        };
+
     default:
       return state;
   }
 }
+const store = createStore(reducer);
+store.dispatch({ type: "account/deposit", payload: 500 });
+store.dispatch({ type: "account/withdraw", payload: 200 });
+store.dispatch({ type: "account/requestLoan", payload: { amount: 1000, purpose: "Buy a car" } });
+store.dispatch({ type: "account/payLoan" });
+
+console.log(store.getState());
